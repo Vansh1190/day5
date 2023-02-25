4d63bd6d84c7f8a2b244945779e6001cd6da8d0b
Vansh <vanshk605@gmail.com>
2023-02-25 06:38:27 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <Navbar title = "HomePage" num = {5} />
+    // <Navbar />
+  );
+}
+
+export default App;
